6ee0a3ec5349372ef13a0508cdd9e573b2e29210
Thomas Gilbert <tcgilbert94@gmail.com>
2021-01-21 10:41:54 -0500

    [App] checking the user against the token in storage every time the app remounts

--- src/App.js
+++ src/App.js
@@ -2,14 +2,33 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
+import axios from 'axios'
+import jwt_decode from 'jwt-decode'
 
 
 // components
 import Signup_Login from "./components/Signup_Login";
 
 function App() {
-    const [currentUser, setCurrentUser] = useState("");
+    const [currentUser, setCurrentUser] = useState(null);
     const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const SERVER = process.env.REACT_APP_SERVER
+
+    useEffect(async () => {
+        const token = localStorage.getItem('jwtToken')
+        if (token) {
+            console.log(token);
+            const tokenUser = await axios.post(`${SERVER}/users/check-token`, {
+                token: token
+            })
+            const userFound = await tokenUser.data.user_found
+            if (userFound) {
+                const userInfo = await jwt_decode(token)
+                setCurrentUser(userInfo)
+                setIsAuthenticated(true)
+            }
+        }
+    }, [])
 
     const handleLogout = () => {
       console.log('logging out');
