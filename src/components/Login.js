788082738c424f0479e5b65d4894b30d863af111
Thomas Gilbert <tcgilbert94@gmail.com>
2021-01-21 10:57:25 -0500

    [Login, SignupLogin] moved handleLogin to parent component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,42 +1,11 @@
 import React from "react";
 import useForm from "../utilities/useForm";
-import axios from 'axios'
-import setAuthToken from '../utilities/setAuthToken'
-import jwt_decode from 'jwt-decode'
 import { Redirect } from "react-router-dom"
 
 
 const Login = (props) => {
     const [values, handleChange] = useForm({ identifier: "", password: "" });
     const SERVER = process.env.REACT_APP_SERVER
-
-    const handleLogin = async () => {
-        try {
-            console.log("logging in");     
-            // look for user
-            const requestedUser = await axios.post(`${SERVER}/users/login`, {
-                email: values.identifier,
-                password: values.password
-            })
-            // extract token
-            const { token } = requestedUser.data
-            // add to local storage
-            localStorage.setItem('jwtToken', token)
-            // set token
-            setAuthToken(token)
-            // decode token
-            const userInfo = jwt_decode(token)
-            // set the current user
-            props.setUser(userInfo)
-            props.setAuth(true)
-
-            // TO DO: redirect to profile component
-
-        } catch (error) {
-            console.log(error);
-            console.log(`LOGIN ERROR: ${error.data}`);
-        }
-    }
 
     return (
         <div>
@@ -55,7 +24,7 @@ const Login = (props) => {
                 value={values.password}
                 onChange={handleChange}
             />
-            <button type="submit" onClick={handleLogin}>Login</button>
+            <button type="submit" onClick={() => props.handleLogin(values)}>Login</button>
         </div>
     );
 };
--- src/components/Signup_Login.js
+++ src/components/Signup_Login.js
@@ -1,13 +1,45 @@
 import React from "react";
 import Login from "./Login";
 import Signup from "./Signup";
+import axios from 'axios'
+import setAuthToken from '../utilities/setAuthToken'
+import jwt_decode from 'jwt-decode'
 
 const Signup_Login = (props) => {
 
+    const SERVER = process.env.REACT_APP_SERVER
+
+    const handleLogin = async (values) => {
+        try {
+            console.log("logging in");     
+            // look for user
+            const requestedUser = await axios.post(`${SERVER}/users/login`, {
+                email: values.identifier,
+                password: values.password
+            })
+            // extract token
+            const { token } = requestedUser.data
+            // add to local storage
+            localStorage.setItem('jwtToken', token)
+            // set token
+            setAuthToken(token)
+            // decode token
+            const userInfo = jwt_decode(token)
+            // set the current user
+            props.setUser(userInfo)
+            props.setAuth(true)
+
+            // TO DO: redirect to profile component
+
+        } catch (error) {
+            console.log(error);
+            console.log(`LOGIN ERROR: ${error.data}`);
+        }
+    }
 
     return (
         <div>
-            <Login setUser={props.setUser} setAuth={props.setAuth}/>
+            <Login handleLogin={handleLogin} setUser={props.setUser} setAuth={props.setAuth}/>
             <Signup setUser={props.setUser} setAuth={props.setAuth}/>
         </div>
     );
